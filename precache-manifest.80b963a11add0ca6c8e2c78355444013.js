6b6fb453a26e7637ad0aab02d6e031511c441016
Saeed Torabzadeh <saeed.torabzadeh@gmail.com>
2020-05-25 17:34:10 +0430

    Updates

--- precache-manifest.80b963a11add0ca6c8e2c78355444013.js
+++ precache-manifest.80b963a11add0ca6c8e2c78355444013.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4767cf1ee51116cbc660ed080ff13a4c",
+    "revision": "151e101d389137ad1055c5585e452b5b",
     "url": "/stock-risk-calculator/index.html"
   },
   {
-    "revision": "57c4812cbf44fadfcd4f",
-    "url": "/stock-risk-calculator/static/css/main.c4e626c5.chunk.css"
+    "revision": "efe11114cc0983bc4322",
+    "url": "/stock-risk-calculator/static/css/main.d3c867ae.chunk.css"
   },
   {
     "revision": "491526ef2f4856d8250a",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/stock-risk-calculator/static/js/2.ce392021.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "57c4812cbf44fadfcd4f",
-    "url": "/stock-risk-calculator/static/js/main.dbc0e894.chunk.js"
+    "revision": "efe11114cc0983bc4322",
+    "url": "/stock-risk-calculator/static/js/main.39bbe568.chunk.js"
   },
   {
     "revision": "55a88925e9d3ca9f0893",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stock-risk-calculator/precache-manifest.b4af2a7bcabafdf5bfeef22864a6e3da.js"
+  "/stock-risk-calculator/precache-manifest.80b963a11add0ca6c8e2c78355444013.js"
 );
 
 self.addEventListener('message', (event) => {
